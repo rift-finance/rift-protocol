aea2771e302619cb5cedbaa7e29347c32fbcb791
balasan <slava@4real.io>
2022-05-03 17:26:57 -0400

    Rift token tests

--- src/token/IRift.sol
+++ src/token/IRift.sol
@@ -7,15 +7,34 @@ import "../../lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/exten
 import "../../lib/openzeppelin-contracts-upgradeable/contracts/governance/utils/IVotesUpgradeable.sol";
 
 interface IRift is IERC20Upgradeable, IERC20PermitUpgradeable, IAccessControlUpgradeable, IVotesUpgradeable {
-    function owner() external;
+    // roles
+    function MINTER_ROLE() external view returns (bytes32);
 
-    function pendingOwner() external;
+    function DEFAULT_ADMIN_ROLE() external view returns (bytes32);
 
-    function burn(uint256 amount) external;
+    function BURNER_ROLE() external view returns (bytes32);
 
-    function burnFrom(address account, uint256 amount) external;
+    // ownership
+
+    function owner() external view returns (address);
+
+    function pendingOwner() external view returns (address);
 
     function transferOwnership(address _pendingOwner) external;
 
     function acceptOwnership() external;
+
+    // mint / burn
+
+    function burn(uint256 amount) external;
+
+    function mint(address account, uint256 amount) external;
+
+    function burnFrom(address account, uint256 amount) external;
+
+    // erc20
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function increaseAllowance(address account, uint256 amount) external returns (uint256);
 }
