a773bad5c30715effd0cdaf08202195c37b198c4
balasan <slava@4real.io>
2022-05-03 13:44:06 -0400

    token-spec implementation

--- src/token/Rift.sol
+++ src/token/Rift.sol
@@ -1,16 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.11;
+pragma solidity ^0.8.11;
 
 import "../../lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "../../lib/openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import "../../lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "../../lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 
-contract Rift is ERC20BurnableUpgradeable {
+contract Rift is
+    Initializable,
+    ERC20Upgradeable,
+    ERC20BurnableUpgradeable,
+    AccessControlUpgradeable,
+    ERC20PermitUpgradeable,
+    ERC20VotesUpgradeable
+{
     address public owner;
-    mapping(address => bool) public isBurner;
-    mapping(address => bool) public isMinter;
+    address public pendingOwner;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    event OwnershipTransferInitiated(address owner, address pendingOwner);
+    event OwnershipTransferred(address oldOwner, address newOwner);
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
 
     function initialize(address _owner) public initializer {
         owner = _owner;
         __ERC20_init("RIFT", "Rift Token");
+        __ERC20Burnable_init();
+        __AccessControl_init();
+        __ERC20Permit_init("Rift Token");
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
+
+        // do we want to init MINTER_ROLE and BURNER_ROLE?
     }
 
     modifier onlyOwner() {
@@ -18,41 +42,55 @@ contract Rift is ERC20BurnableUpgradeable {
         _;
     }
 
-    modifier onlyMinter() {
-        require(isMinter[msg.sender], "ONLY_MINTER");
-        _;
+    // ----------- Ownership -----------
+
+    /// @dev Init transfer of ownership of the contract to a new account (`_pendingOwner`).
+    /// @param _pendingOwner pending owner of contract
+    /// Can only be called by the current owner.
+    function transferOwnership(address _pendingOwner) external onlyOwner {
+        pendingOwner = _pendingOwner;
+        // important to grant admin role to pendingOwner here
+        _grantRole(DEFAULT_ADMIN_ROLE, pendingOwner);
+        emit OwnershipTransferInitiated(owner, pendingOwner);
     }
 
-    modifier onlyBurner() {
-        require(isBurner[msg.sender], "ONLY_BURNER");
-        _;
+    function acceptOwnership() external {
+        require(msg.sender == pendingOwner, "NOT_PENDING_OWNER");
+        address oldOwner = owner;
+        owner = pendingOwner;
+
+        // revoke the DEFAULT ADMIN ROLE from prev owner
+        _revokeRole(DEFAULT_ADMIN_ROLE, oldOwner);
+
+        emit OwnershipTransferred(oldOwner, owner);
     }
 
-    function addMinter(address _newMinter) public onlyOwner {
-        require(!isMinter[_newMinter], "ALREADY_MINTER");
-        isMinter[_newMinter] = true;
-    }
+    // ----------- Mint / Burn -----------
+    // note: users can burn their tokens
 
-    function addBurner(address _newBurner) public onlyOwner {
-        require(!isBurner[_newBurner], "ALREADY_BURNER");
-        isBurner[_newBurner] = true;
-    }
-
-    function revokeMinter(address _oldMinter) public onlyOwner {
-        require(isMinter[_oldMinter], "NOT_MINTER");
-        isMinter[_oldMinter] = false;
-    }
-
-    function revokeBurner(address _oldBurner) public onlyOwner {
-        require(isBurner[_oldBurner], "NOT_BURNER");
-        isBurner[_oldBurner] = false;
-    }
-
-    function mint(address account, uint256 amount) public onlyMinter {
+    function mint(address account, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(account, amount);
     }
 
-    function burnFrom(address account, uint256 amount) public override onlyBurner {
+    function burnFrom(address account, uint256 amount) public override onlyRole(BURNER_ROLE) {
         super.burnFrom(account, amount);
+    }
+
+    // ----------- The following functions are overrides required by Solidity. -----------
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal override(ERC20Upgradeable, ERC20VotesUpgradeable) {
+        super._burn(account, amount);
     }
 }
