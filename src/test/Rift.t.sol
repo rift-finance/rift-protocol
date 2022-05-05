94bec928571ee20ae2424f6bfea5826aa7fa4f86
balasan <slava@4real.io>
2022-05-05 14:25:23 -0400

    fixed version

--- src/test/Rift.t.sol
+++ src/test/Rift.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.11;
+pragma solidity 0.8.11;
 
 import { TransparentUpgradeableProxy } from "../../lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import { ProxyAdmin } from "../../lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
