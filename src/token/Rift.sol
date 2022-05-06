950b005f4ae18a37a253ee705d886ea28b1548ac
slava <slava@4real.io>
2022-05-06 12:42:40 -0400

    Rift Token Spec (#11)
    * token-spec implementation
    
    * update Rift token tests
    
    * Rift token tests
    
    * address pr review
    
    * Safe bootstrap (#12)
    
    * token-spec implementation
    
    * update Rift token tests
    
    * Rift token tests
    
    * address pr review
    
    * add foundry config template
    
    * gitignore foundry.toml
    
    * consistent env var name
    
    * add optional support for a .env file
    
    * fork from master
    
    * token-spec implementation
    
    * update Rift token tests
    
    * Rift token tests
    
    * address pr review
    
    * wip
    
    * fixed version
    
    * rm ERC20BUrnableUpgradeable
    
    * rm .tool-versions

--- src/token/Rift.sol
+++ src/token/Rift.sol
@@ -1,16 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.11;
 
-import "../../lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "../../lib/openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import "../../lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
 
-contract Rift is ERC20BurnableUpgradeable {
+import "./IRift.sol";
+
+contract Rift is AccessControlUpgradeable, ERC20VotesUpgradeable {
+    uint216[400] private __gap; // in case we want to extend the contract in the future
     address public owner;
-    mapping(address => bool) public isBurner;
-    mapping(address => bool) public isMinter;
+    address public pendingOwner;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    event OwnershipTransferInitiated(address owner, address pendingOwner);
+    event OwnershipTransferred(address oldOwner, address newOwner);
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
 
     function initialize(address _owner) public initializer {
         owner = _owner;
         __ERC20_init("RIFT", "Rift Token");
+        __AccessControl_init();
+        __ERC20Permit_init("Rift Token");
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
     }
 
     modifier onlyOwner() {
@@ -18,41 +32,42 @@ contract Rift is ERC20BurnableUpgradeable {
         _;
     }
 
-    modifier onlyMinter() {
-        require(isMinter[msg.sender], "ONLY_MINTER");
-        _;
+    // ----------- Ownership -----------
+
+    /// @dev Init transfer of ownership of the contract to a new account (`_pendingOwner`).
+    /// @param _pendingOwner pending owner of contract
+    /// Can only be called by the current owner.
+    function transferOwnership(address _pendingOwner) external onlyOwner {
+        pendingOwner = _pendingOwner;
+        emit OwnershipTransferInitiated(owner, pendingOwner);
     }
 
-    modifier onlyBurner() {
-        require(isBurner[msg.sender], "ONLY_BURNER");
-        _;
+    /// @dev Accept transfer of ownership of the contract.
+    /// Can only be called by the pendingOwner.
+    function acceptOwnership() external {
+        require(msg.sender == pendingOwner, "ONLY_PENDING_OWNER");
+        address oldOwner = owner;
+        owner = pendingOwner;
+
+        // revoke the DEFAULT ADMIN ROLE from prev owner
+        _revokeRole(DEFAULT_ADMIN_ROLE, oldOwner);
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
+
+        emit OwnershipTransferred(oldOwner, owner);
     }
 
-    function addMinter(address _newMinter) public onlyOwner {
-        require(!isMinter[_newMinter], "ALREADY_MINTER");
-        isMinter[_newMinter] = true;
-    }
+    // ----------- Mint / Burn -----------
+    // note: users can burn their tokens
 
-    function addBurner(address _newBurner) public onlyOwner {
-        require(!isBurner[_newBurner], "ALREADY_BURNER");
-        isBurner[_newBurner] = true;
-    }
-
-    function revokeMinter(address _oldMinter) public onlyOwner {
-        require(isMinter[_oldMinter], "NOT_MINTER");
-        isMinter[_oldMinter] = false;
-    }
-
-    function revokeBurner(address _oldBurner) public onlyOwner {
-        require(isBurner[_oldBurner], "NOT_BURNER");
-        isBurner[_oldBurner] = false;
-    }
-
-    function mint(address account, uint256 amount) public onlyMinter {
+    /// @dev Mints tokens to an account.
+    /// Can only be called by MINTER_ROLE.
+    function mint(address account, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(account, amount);
     }
 
-    function burnFrom(address account, uint256 amount) public override onlyBurner {
-        super.burnFrom(account, amount);
+    /// @dev Destroys `amount` tokens from the caller.
+    /// See {ERC20-_burn}.
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
     }
 }
